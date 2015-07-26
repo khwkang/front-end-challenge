19b40481b03c925729f51e54ad3dde5e366cde6a
Ken <khwken@gmail.com>
2015-07-26 16:57:10 -0700

    [JS] +pseudo-classical +replace setTimeout with chained ajax request resolving dependencies +code clean-ups and styling

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
+(function(global) {
 
-function domobj(){
-  var self        =this;
-  self.products   = [];
+  var requestData = $.getJSON('data.json').done(function(products) {
+    var requestProductTemplate = $.get('product-template.html').done(function(template) {
 
-  self.getproducts = function(url){
-    $.getJSON(url, function(response){
-        for(i=0; i<response.sales.length ; i++){
-          self.products.push( new productobj(response.sales[i], i)  );
+      var data = products;
+      var template = template;
+
+      function TOMApp() {
+        this.products = [];
+      }
+
+      TOMApp.prototype.getproducts = function() {
+        var self = this;
+        for (i=0; i<data.sales.length ; i++) {
+          self.products.push(new productobj(data.sales[i], i)  );
         }
+      }
+
+      TOMApp.prototype.updateproducthtml = function() {
+        for( i=0; i< this.products.length ; i++){
+          this.products[i].updatehtml();
+        }
+      }
+
+      TOMApp.prototype.updatedom = function() {
+        thishtml=''; 
+        for(var i=0; i< this.products.length ; i++){
+          
+          if (i % 3 == 0 ) {  
+            thishtml += "<div class='row'>"; 
+            console.log("START");
+          }
+
+          thishtml += this.products[i].htmlview;
+          if ((i % 3 == 2) || i == (this.products.length-1) ) {
+            thishtml += "</div>";
+            console.log("FINISH");
+          }
+        }
+        $("#content").append(thishtml)
+      }
+
+      function productobj(product, i) {
+        this.photo        = product.photos.medium_half;
+        this.title        = product.name;
+        this.tagline      = product.tagline;
+        this.url          = product.url;
+        this.htmlview     = "";
+        this.index        = i;
+        this.custom_class = "col"+ ((i % 3) +1);
+      }
+
+      productobj.prototype.updatehtml = function() {
+        var self = this;
+        self.htmlview = 
+          template.replace('{image}', self.photo)
+                  .replace('{title}', self.title)
+                  .replace('{tagline}', self.tagline)
+                  .replace('{url}', self.url)
+                  .replace('{custom_class}', self.custom_class);
+      }
+
+      var page = new TOMApp();
+      page.getproducts();
+      page.updateproducthtml();
+      page.updatedom();
     });
-  }
-    
-  self.updateproducthtml = function(){
-    for( i=0; i< self.products.length ; i++){
-      self.products[i].updatehtml();
-    }
-  }
-  
-  self.updatedom = function(){
-    var i=0
-    thishtml='';
-    for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row'>"; console.log("START") }
-      thishtml += self.products[i].htmlview;
-      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
-    }
-    $("#content").append(thishtml)
-  }
-  
-}
+  });
 
-function productobj(product, i){
-  var self          = this;
-  self.photo        = product.photos.medium_half
-  self.title        = product.name
-  self.tagline      = product.tagline
-  self.url          = product.url
-  self.htmlview     = ""
-  self.index        = i
-  self.custom_class = "col"+ ((i % 3) +1)
-  
-  self.updatehtml= function(){
-    $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
-    });
-  }
-}
-
-
-var page=new domobj();
-page.getproducts('data.json');
-setTimeout("console.log('building html');page.updateproducthtml();",20);
-setTimeout("page.updatedom()",50)
+}(this));
