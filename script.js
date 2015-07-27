7e660b94f52a08aa136505bfeb5956ed82f741bf
Ken <khwken@gmail.com>
2015-07-26 21:41:23 -0700

    [JS] +implement simple resource loader with detailed annotation +refactor application main script using resource loader

--- script.js
+++ script.js
@@ -1,97 +1,98 @@
 (function(global) {
 
+  // tempalating functing 
   var _template = function(str, param) {
     return str.replace(/\{(.*?)\}/g, function(match, token) {
       return param[token];
     });
   }
 
-  var requestData = $.getJSON('data.json').done(function(productData) {
-    var requestProductTemplate = $.get('product-template.html').done(function(productTemplateHtml) {
+  // self implemented resource loader to fulfill dependencies
+  // ** please refer to 'tiny-require.js' for annotated source code. 
+  tinyRequire(['data.json', 'product-template.html'], function(productData, productTemplateHtml) {
 
-      function TOMApp() {
-        var self = this;
+    function TOMApp() {
+      var self = this;
 
-        self.regions = {
-          "$mainRegion": $("#content")
-        }
-
-        self.products = [];
-
-        self.initProducts();
-        self.render();
+      self.regions = {
+        "$mainRegion": $("#content")
       }
 
-      TOMApp.prototype.initProducts = function() {
-        var self = this;
+      self.products = [];
 
-        for (var i=0; i<productData.sales.length ; i++) {
-          self.products.push(new Product(productData.sales[i], i)  );
-        }
+      self.initProducts();
+      self.render();
+    }
+
+    TOMApp.prototype.initProducts = function() {
+      var self = this;
+
+      for (var i=0; i<productData.sales.length ; i++) {
+        self.products.push(new Product(productData.sales[i], i));
       }
+    }
 
-      TOMApp.prototype.render = function() {
-        var self = this;
+    TOMApp.prototype.render = function() {
+      var self = this;
 
-        self.regions.$mainRegion.empty();
+      self.regions.$mainRegion.empty();
 
-        var $row = null;
-        for(var i=0; i< self.products.length ; i++){
-          
-          if (i % 3 == 0 ) {  
-            $row = $("<div>").addClass("row");
-            console.log("START");
-          }
-
-          $row.append(self.products[i].render());
-
-          if ((i % 3 == 2) || i == (self.products.length-1)) {
-            self.regions.$mainRegion.append($row);
-            console.log("FINISH");
-          }
-        }
-      }
-
-      function Product(product, i) {
-        var self = this;
-
-        self.photo        = product.photos.medium_half;
-        self.title        = product.name;
-        self.tagline      = product.tagline;
-        self.url          = product.url;
-        self.index        = i;
-        self.custom_class = "col"+ ((i % 3) +1);
-
-        self.$elem = null;
-      }
-
-      Product.prototype.render = function() {
-        var self = this;
-
-        var param = {
-          image: self.photo,
-          title: self.title,
-          tagline: self.tagline,
-          url: self.url,
-          custom_class: self.custom_class
+      var $row = null;
+      for(var i=0; i< self.products.length ; i++){
+        
+        if (i % 3 == 0 ) {
+          $row = $("<div>").addClass("row");
+          console.log("START");
         }
 
-        var htmlView = _template(productTemplateHtml, param);
+        $row.append(self.products[i].render());
 
-        if (self.$elem) {
-          // instantiated already, just update html
-          self.$elem.html(htmlView);
-        } else {
-          // Instantiation of the jQuery object
-          self.$elem = $(htmlView);
+        if ((i % 3 == 2) || i == (self.products.length-1)) {
+          self.regions.$mainRegion.append($row);
+          console.log("FINISH");
         }
+      }
+    }
 
-        return self.$elem;
+    function Product(product, i) {
+      var self = this;
+
+      self.photo        = product.photos.medium_half;
+      self.title        = product.name;
+      self.tagline      = product.tagline;
+      self.url          = product.url;
+      self.index        = i;
+      self.custom_class = "col"+ ((i % 3) +1);
+
+      self.$elem = null;
+    }
+
+    Product.prototype.render = function() {
+      var self = this;
+
+      var param = {
+        image: self.photo,
+        title: self.title,
+        tagline: self.tagline,
+        url: self.url,
+        custom_class: self.custom_class
       }
 
-      // Entry point
-      var app = new TOMApp();
-    });
+      var htmlView = _template(productTemplateHtml, param);
+
+      if (self.$elem) {
+        // instantiated already, just update html
+        self.$elem.html(htmlView);
+      } else {
+        // Instantiation of the jQuery object
+        self.$elem = $(htmlView);
+      }
+
+      return self.$elem;
+    }
+
+    // Entry point
+    var app = new TOMApp();
   });
 
 }(this));
